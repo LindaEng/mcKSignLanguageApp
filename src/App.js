d82e7a55d3ef03bd6b0007705cb4d0afa66d29be
Boymeetsworld <aydenscott444@gmail.com>
2022-01-01 15:52:17 -0500

    redesinged interface, added randomizer function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,83 @@
-import React, {useState} from "react";
-import FlashcardList from './Components/FlashcardList';
+import React, { Component } from "react";
 import './App.css'
+import Card from "./card/card";
+import DrawButton from './draw button/drawButton';
 
-function App() {
-  const [cards, setCards] = useState(SAMPLE_CARDS)
+class App extends Component {
+  constructor(props){
+    super(props);
 
-  return (
-    <FlashcardList flashcards={cards} />
-  );
+    this.updateCard = this.updateCard.bind(this);
+//created state
+    this.state = {
+      cards: [
+        {id: 1, eng:"A", asl: "Aye" },
+        {id: 2, eng:"B", asl: "Bee"},
+        {id: 3, eng:"C", asl: "See" },
+        {id: 4, eng:"D", asl: "Dee"},
+        {id: 5, eng:"E", asl: "Eyy" },
+        {id: 6, eng:"F", asl: "Eff"},
+        {id: 7, eng:"G", asl: "Gee" },
+        {id: 8, eng:"H", asl: "Aycha"},
+        {id: 9, eng:"I", asl: "Eye" },
+        {id: 10, eng:"J", asl: "Jay"},
+        {id: 11, eng:"K", asl: "Kay" },
+        {id: 12, eng:"L", asl: "El"},
+        {id: 13, eng:"M", asl: "Em" },
+        {id: 14, eng:"N", asl: "En"},
+        {id: 15, eng:"O", asl: "Oh" },
+        {id: 16, eng:"P", asl: "Pee"},
+        {id: 17, eng:"Q", asl: "Cue" },
+        {id: 18, eng:"R", asl: "Arr"},
+        {id: 19, eng:"S", asl: "Es" },
+        {id: 20, eng:"T", asl: "Tee"},
+        {id: 21, eng:"U", asl: "You" },
+        {id: 22, eng:"V", asl: "Vee"},
+        {id: 23, eng:"W", asl: "Double u" },
+        {id: 24, eng:"X", asl: "Ex"},
+        {id: 25, eng:"Y", asl: "Why" },
+        {id: 26, eng:"Z", asl: "Zee"},
+      ],
+      currentCard: {}
+    }
+  }
+  
+componentWillMount(){
+  const currentCards = this.state.cards;
+
+
+this.setState({
+  cards: currentCards,
+  currentCard: this.getRandomCard(currentCards)
+})
 }
 
-const SAMPLE_CARDS = [
-  {
-    id: 1,
-    question: 'Capital of France',
-    answer: 'Paris',
-    options: ['London', 'Paris', 'Barcelona']
-  },
-  {
-    id: 2,
-    question: 'Capital of England',
-    answer: 'London',
-    options: ['London', 'Paris', 'Barcelona'] 
-  },
-  {
-   id: 3,
-   question: 'whats my name?',
-   answer: 'Ayden',
-   options: ['Michael', 'Simon', 'Ayden']
-  }
-]
+getRandomCard(currentCards){
+  var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+  return(card);
+}
 
+updateCard(){
+  const currentCards = this.state.cards;
+this.setState({currentCard: this.getRandomCard(currentCards)
+})
+}
+
+
+  render()  {
+    return (
+      <div className="App">
+        <div className="cardRow">
+        <Card eng={this.state.currentCard.eng}
+              asl={this.state.currentCard.asl}
+              />
+      </div>
+      <div className="buttonRow">
+      <DrawButton drawCard={this.updateCard}/>
+      </div>
+    </div>
+    );
+  }
+
+}
 export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export  const DB_CONFIG = {
+    apiKey: "AIzaSyAMCTfdbj24gQodjK1MnF3XoLu3uBixgCw",
+    authDomain: "flashcards-568aa.firebaseapp.com",
+    projectId: "flashcards-568aa",
+    storageBucket: "flashcards-568aa.appspot.com",
+    messagingSenderId: "355842740621",
+    appId: "1:355842740621:web:91f5b247bed1e6c0c5edc4",
+    measurementId: "G-169F5DRDGT"
+  };
