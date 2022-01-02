d409854bd626090e377fa7266034f922adbc342d
datacreator001 <amirrorphone@gmail.com>
2022-01-02 11:22:06 -0500

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FlashcardList from './components/Views/FlashcardList';
 import Flashcards from './components/Views/Flashcards';
 import Homepage from './components/Views/Homepage';
+import Dictionary from './components/Views/Dictionary';
+import TrainingRoom from './components/Views/Trainingroom';
 import './App.css';
 
 function App() {
@@ -10,10 +12,14 @@ function App() {
 		<Router>
 			<nav className="nav">
 				<Link to='/' className="links">Home</Link>
-				<Link to='/FlashcardList' className="links">FlashFlashcards List</Link>
+				<Link to='/FlashcardList' className="links">Flashcards List</Link>
+				<Link to='/Dictionary' className="links">Dictionary</Link>
+				<Link to='/TrainingRoom' className="links">Training Room</Link>
 			</nav>
 			<Routes>
 				<Route path='/FlashcardList' element={<FlashcardList />} />
+				<Route path='/Dictionary' element={<Dictionary/>}/>
+				<Route path='/TrainingRoom' element={<TrainingRoom/>}/>
 
 				<Route path='/' element={<Homepage />} />
 			</Routes>
